af875091e75902341d49cd4bd16a3e93fdf3fcda
Naayeon <naaay097@gmail.com>
2023-05-15 23:22:28 +0900

    Add e-7 풀이

--- e-4/nayeon.ts
+++ e-4/nayeon.ts
@@ -1,3 +1,11 @@
-type MyPick<T, K extends keyof T> = {
-  [k in K]: T[k];
+// T의 모든 프로퍼티를 읽기 전용(재할당 불가)으로 바꾸는 내장 제네릭 Readonly<T>를 이를 사용하지 않고 구현
+
+// 오답
+type MyReadonlyIncorrect<T> = {
+  readonly [k in T]: T[k];
+};
+
+// 정답
+type MyReadonlyCorrect<T> = {
+  readonly [k in keyof T]: T[k];
 };
