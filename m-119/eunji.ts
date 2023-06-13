8322df2202cf43d16384f9e9a70f86782241ee35
유은지 <aine0227@naver.com>
2023-06-13 15:28:26 +0900

    Create eunji.ts

---
+++ m-119/eunji.ts
@@ -0,0 +1,4 @@
+type ReplaceAll<S extends string, From extends string, To extends string> = 
+  From extends '' ? S : S extends `${infer A}${From}${infer B}`
+  ? `${A}${To}${ReplaceAll<B, From, To>}`
+  : S
